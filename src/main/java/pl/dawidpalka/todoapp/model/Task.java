8f941001ea752a1137cbeedb3bbddae3c0d6027a
Dawid Pałka <dpalka93@gmail.com>
2021-12-06 01:29:20 +0100

    task model and repository

---
+++ src/main/java/pl/dawidpalka/todoapp/model/Task.java
@@ -0,0 +1,27 @@
+package pl.dawidpalka.todoapp.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+    @NotBlank
+    private String description;
+    private boolean done;
+}
